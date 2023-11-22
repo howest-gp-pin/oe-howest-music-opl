e37c0235580cf8dea8b1f0ab1329fe1ab4d14920
Maxim Lesy <maxim.lesy@howest.be>
2023-11-22 19:52:03 +0100

    Add time input

--- Pin.HowestMusic/Models/Track.cs
+++ Pin.HowestMusic/Models/Track.cs
@@ -5,15 +5,14 @@ namespace Pin.HowestMusic.Models
     public class Track
     {
         public Guid Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public string Artist { get; set; }
-        //public List<Genre> Genre { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public string Artist { get; set; }
+
+        [Required] public TimeOnly Duration { get; set; }
         public DateTime ReleaseDate { get; set; }
-        [Range(0, 5)]
-        public int Rating { get; set; }
+        [Range(0, 5)] public int Rating { get; set; }
         public int PlayCount { get; set; }
         public bool IsFavorite { get; set; }
+        //public List<Genre> Genre { get; set; }
     }
     }
