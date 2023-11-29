51d89a0b1af20201766933cad2f8b3d157ef92b0
Maxim Lesy <maxim.lesy@howest.be>
2023-11-29 20:12:22 +0100

    Add JS Interop

---
+++ Pin.HowestMusic/Components/ApplicationComponentBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Pin.HowestMusic.Components
+{
+    public abstract class ApplicationComponentBase : ComponentBase
+    {
+        public int TimesDisplayed { get; set; }
+    }
+}
---
+++ Pin.HowestMusic/Pages/Index.razor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pin.HowestMusic.Models;
+using Pin.HowestMusic.Services.Interfaces;
+namespace Pin.HowestMusic.Pages
+{
+    public partial class Index
+    {
+        private Genre[] allGenres;
+        private Track[] allTracks;
+        private Track[] favouriteTrackList;
+
+        private Track currentTrack = null;
+
+        //using init keyword, introduced in C# 9
+        [Inject]
+        public ICrudService<Genre> GenreService { get; init; }
+
+        [Inject]
+        public ICrudService<Track> TrackService { get; init; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await RefreshTracks();
+            allGenres = (await GenreService.GetAllAsync()).ToArray();
+        }
+
+        private void AddTrack()
+        {
+            currentTrack = new Track();
+        }
+
+        private async void SaveTrack(Track track)
+        {
+            if (track.Id == Guid.Empty)
+            {
+                await TrackService.CreateAsync(track);
+            }
+            else
+            {
+                await TrackService.UpdateAsync(track);
+            }
+            await RefreshTracks();
+            currentTrack = null;
+        }
+
+        private async void EditTrack(Track trackToEdit)
+        {
+            currentTrack = trackToEdit;
+        }
+
+        private async void RemoveTrack(Track trackToDelete)
+        {
+            await TrackService.DeleteAsync(trackToDelete.Id);
+            await RefreshTracks();
+        }
+
+        private async Task RefreshTracks()
+        {
+            allTracks = (await TrackService.GetAllAsync()).ToArray();
+        }
+
+        private void Cancel()
+        {
+            currentTrack = null;
+        }
+    }
+}
